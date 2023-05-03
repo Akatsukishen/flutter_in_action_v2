5123814e40a324d22632cca282f5c8d5932f7f1f
li.shensong <lishensong@126.com>
2023-05-03 11:42:40 +0800

    feat:放大动画

---
+++ lib/ch09/scale_animation_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ScaleAnimationPage extends StatefulWidget {
+  const ScaleAnimationPage({Key? key}) : super(key: key);
+
+  @override
+  State<ScaleAnimationPage> createState() => _ScaleAnimationPageState();
+}
+
+class _ScaleAnimationPageState extends State<ScaleAnimationPage>
+    with SingleTickerProviderStateMixin {
+  //需要监听屏幕刷新
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    //动画控制器
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    //动画值区间
+    animation = Tween(begin: 0.0, end: 300.0).animate(controller)
+      ..addListener(() {
+        //监听每一帧的变化
+        setState(() {});
+      });
+    //启动动画
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('放大动画'),
+      ),
+      body: Center(
+          child: Image.asset(
+        'assets/images/qiaoba.jpeg',
+        width: animation.value,
+      )),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();//防止内存泄漏
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_in_action_v2/ch04/fractional_offset_page.dart';
 import 'package:flutter_in_action_v2/ch04/layout_builder_page.dart';
 import 'package:flutter_in_action_v2/ch05/decorated_box_page.dart';
 import 'package:flutter_in_action_v2/ch05/transform_page.dart';
+import 'package:flutter_in_action_v2/ch09/scale_animation_page.dart';
 
 import 'ch04/align_page.dart';
 
@@ -29,6 +30,7 @@ class HomeEntryPage extends StatelessWidget {
   HomeEntryPage({super.key});
 
   final _entries = <String, WidgetBuilder>{
+    '放大动画' : (context) => const ScaleAnimationPage(),
     'Transform使用': (context) => const TransformPage(),
     'DecoratedBox使用': (context) => const DecoratedBoxPage(),
     'LayoutBuilder使用': (context) => const LayoutBuilderPage(),
