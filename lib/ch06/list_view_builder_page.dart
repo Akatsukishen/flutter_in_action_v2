4745120ef6a17797d52d1586954e1ef1a3ddd09d
li.shensong <lishensong@126.com>
2023-05-13 20:17:37 +0800

    builder方式创建ListView

---
+++ lib/ch06/list_view_builder_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../base/base_page.dart';
+
+class LiveViewBuilderPage extends BasePage {
+  const LiveViewBuilderPage(
+      {super.key, super.title = '使用builder方式来创建ListView'});
+
+  @override
+  Widget buildBody(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context,index) {
+        return ListTile(
+          title: Text('$index'),
+        );
+      },
+      itemExtent: 50, ///强制高度为50
+      itemCount: 100, ///列表项个数
+    );
+  }
+}
